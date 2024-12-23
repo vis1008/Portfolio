1af9e467079746455158c97af70522a61e2cf2db
vishal sultanpur <vishal64594@gmail.com>
2024-12-24 00:27:35 +0530

    Initial commit with project files

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings: ["Frontend Developer", "Data Analyst", "Prompt Engineer"],
+    typespeed: 100,
+    backspeed: 100,
+    backdelay: 1000,
+    loop: true
+})
